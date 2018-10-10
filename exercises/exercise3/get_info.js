efb651099a8082c71f4f1b73677a06756ba1bae6
Hans Larson <you@example.com>
2018-10-10 17:38:57 -0500

    update

--- exercises/exercise3/get_info.js
+++ exercises/exercise3/get_info.js
@@ -1,4 +1,27 @@
+async function getData(url) {
+    return fetch(url)
+        .then(response => response.json())
+        // .then(json => console.log(json))
+        .catch(error => console.log(error));
+}
 
+async function populate() {
+    let [nums] = await Promise.all([
+        getData('https://numbersapi.com/4')
+    ]);
 
+    // console.log(posts);
+    // console.log(users);
+    let postsDiv = document.querySelector('#nums');
 
+    for (let num of nums) {
+        let numId = num.id;
+        let numTitle = num.title;
+        let numBody = num.body;
+        // console.log(postTitle);
 
+        let numDiv = document.createElement('div');
+        postDiv.classList.add('container', 'border', 'border-dark', 'rounded', 'mt-3');
+        
+    }
+}
