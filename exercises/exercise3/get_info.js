60edf2b2600573f98e56b2ca3adf94c31dc626de
Hans Larson <you@example.com>
2018-10-11 19:32:05 -0500

    update

--- exercises/exercise3/get_info.js
+++ exercises/exercise3/get_info.js
@@ -1,27 +1,38 @@
+/* jshint esversion: 6 */
+/* jshint node: true */
+'use strict';
+
+
+
 async function getData(url) {
     return fetch(url)
         .then(response => response.json())
         // .then(json => console.log(json))
         .catch(error => console.log(error));
 }
-
+/*
 async function populate() {
-    let [nums] = await Promise.all([
-        getData('https://numbersapi.com/4')
-    ]);
+    let [number] = await Promise.all([
+        getData('http://numbersapi.com/random/year?json')]);
 
-    // console.log(posts);
-    // console.log(users);
-    let postsDiv = document.querySelector('#nums');
-
-    for (let num of nums) {
-        let numId = num.id;
-        let numTitle = num.title;
-        let numBody = num.body;
-        // console.log(postTitle);
-
-        let numDiv = document.createElement('div');
-        postDiv.classList.add('container', 'border', 'border-dark', 'rounded', 'mt-3');
-        
-    }
+    console.log(number);
+   
 }
+*/
+async function clickedOn(){
+    var input = document.getElementById('input_num').value;
+    var input1 = +input + +1;
+    var input2 = input-1;
+    var number = await Promise.all([getData(`http://numbersapi.com/${input}?json`)]);
+    var number1 = await Promise.all([getData(`http://numbersapi.com/${input1}?json`)]);
+    var number2 = await Promise.all([getData(`http://numbersapi.com/${input2}?json`)]);
+
+    
+    console.log(input);
+    console.log(number);
+    console.log(number1);
+    console.log(number2);
+
+    document.getElementById
+}
+
