2de491059beec7140391d763ee609ae4df8aec6c
larsha03 <larsha03@luther.edu>
2018-12-05 23:05:25 -0800

    Update

--- exercises/exercise5/exercise5.py
+++ exercises/exercise5/exercise5.py
@@ -26,21 +26,23 @@ def get_data_from_db(query):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    order = request.form.get("order")
     if request.method == "GET":
-        return render_template("index.html")    
-        
+        return render_template("index.html")
+            
     if request.form.get("continent"):
         continent = request.form.get("continent")
-        order = request.form.get("order")
-        query = f"select * from nfl where year='{continent}' order by {order};"
+        
+        query = f"select * from nfl_combine2 where year='{continent}' order by {order};"
 
     if request.form.get("region"):
         region = request.form.get("region")
-        query = f"select * from nfl where round='{region}';"
+        order = request.form.get("order")
+        query = f"select * from nfl_combine2 where round='{region}' order by {order};"
 
     if request.form.get("country"):
         country = request.form.get("country")
-        query = f"select * from nfl where position='{country}';"
+        query = f"select * from nfl_combine2 where position='{country}' order by {order};"
 
 
 
@@ -50,27 +52,27 @@ def index():
 @app.route("/continent", methods=["GET","POST"])
 def continent():
 
-    all_continents = get_data_from_db("select DISTINCT year from nfl;")
+    all_continents = get_data_from_db("select DISTINCT year from nfl_combine2;")
     continent = 2012
-    query = f"select * from nfl where year='{continent}' order by fortyyd;"
+    query = f"select * from nfl_combine2 where year='{continent}' order by fortyyd;"
     result = get_data_from_db(query)
     return render_template("continent.html", options=all_continents, rows=result)  
 
 @app.route("/region", methods=["GET", "POST"])
 def region():
 
-    all_regions = get_data_from_db("select DISTINCT round from nfl;")
+    all_regions = get_data_from_db("select DISTINCT round from nfl_combine2;")
     region = 3
-    query = f"select * from nfl where round='{region}';"
+    query = f"select * from nfl_combine2 where round='{region}';"
     result = get_data_from_db(query)
     return render_template("region.html", options=all_regions, rows=result)
 
 
 @app.route("/country", methods=["GET", "POST"])
 def country():
-    all_countries = get_data_from_db("select DISTINCT position from nfl;")
+    all_countries = get_data_from_db("select DISTINCT position from nfl_combine2;")
     country = "CB"
-    query = f"select * from nfl where position='{country}';"
+    query = f"select * from nfl_combine2 where position='{country}';"
     result = get_data_from_db(query)
     return render_template("country.html", options=all_countries, rows=result)
 
